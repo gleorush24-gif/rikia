ab91608c4ba6ae762d52309327fdb170a65e2704
Gordon Laiseniasaenile <gleorush24@Gordons-MacBook-Air.local>
2026-05-31 21:56:30 +1000

    Feature: profile picture upload with camera icon

--- mobile/build/web/flutter_bootstrap.js
+++ mobile/build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3417653775" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2500620125" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
