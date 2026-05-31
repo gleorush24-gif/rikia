b85511ff2481bfed791501da0afc6e0cc4f36f73
Gordon Laiseniasaenile <gleorush24@Gordons-MacBook-Air.local>
2026-05-31 19:04:55 +1000

    Style: light theme with Copilot gradient colors

--- mobile/build/web/flutter_bootstrap.js
+++ mobile/build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "345863827" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2819167925" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
